e9b455e4a76eae6da8620e11de74deb71ba37a80
liguifa <liguifa18840848462@outlook.com>
2015-03-20 18:31:57 +0800

    2015-03-20

---
+++ SportsVenueBooking/SportsVenueBookingBLL/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsVenueBookingBLL
+{
+    public class User:BaseBLL<SportsVenueBookingCommon.Models.User>
+    {
+    }
+}
