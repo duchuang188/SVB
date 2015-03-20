0c0585e2748d3db83bc8829ceac258367feed8e4
liguifa <liguifa18840848462@outlook.com>
2015-03-20 20:13:20 +0800

    2015-03-20(2)

---
+++ SportsVenueBooking/SportsVenueBookingCommon/Enum.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 作者：李贵发
+ * 时间：2015-03-20
+ * 文件：Enum.cs
+ * 功能：枚举参数
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsVenueBookingCommon
+{
+    public class Enum
+    {
+        /// <summary>
+        /// 用户枚举
+        /// </summary>
+        public static enum userType { admin,student,teacher};
+    }
+}
